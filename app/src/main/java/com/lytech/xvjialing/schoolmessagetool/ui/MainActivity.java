e75941cc5146d1c34e4a986d112e32f21bd2d7fb
xvjialing <xvjialing@outlook.com>
2018-01-02 09:19:41 +0800

    first commit

---
+++ app/src/main/java/com/lytech/xvjialing/schoolmessagetool/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lytech.xvjialing.schoolmessagetool.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lytech.xvjialing.schoolmessagetool.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
